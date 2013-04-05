41e997ffb3ea1425c12c2a4139c7e6e7745fb5f0
Satoshi Azuma <ytnobody@gmail.com>
2013-04-05 09:34:57 +0000

    Import of YTURTLE/WebService-Livedoor-Weather-0.03 from CPAN.
    gitpan-cpan-distribution: WebService-Livedoor-Weather
    gitpan-cpan-version:      0.03
    gitpan-cpan-path:         YTURTLE/WebService-Livedoor-Weather-0.03.tar.gz
    gitpan-cpan-author:       YTURTLE
    gitpan-cpan-maturity:     released

--- lib/WebService/Livedoor/Weather.pm
+++ lib/WebService/Livedoor/Weather.pm
@@ -2,82 +2,89 @@ package WebService::Livedoor::Weather;
 
 use strict;
 use warnings;
+use utf8;
 use Carp;
 use URI::Fetch;
 use XML::Simple;
+use JSON 2;
+our $VERSION = '0.03';
 
-our $VERSION = '0.02';
+use constant BASE_URI        => $ENV{LDWEATHER_BASE_URI} || 'http://weather.livedoor.com';
+use constant ENDPOINT_URI    => $ENV{LDWEATHER_ENDPOINT_URI} || BASE_URI. '/forecast/webservice/json/v1';
+use constant FORECASTMAP_URI => $ENV{LDWEATHER_FORECASTMAP_URI} || BASE_URI. '/forecast/rss/primary_area.xml';
 
 sub new {
     my ( $class, %args ) = @_;
     $args{fetch} ||= {};
     $args{fetch} = {
-	%{$args{fetch}},
-        UserAgent => LWP::UserAgent->new( agent => "WebService::Livedoor::Weather/$VERSION" )
+        %{$args{fetch}},
+        UserAgent => LWP::UserAgent->new( agent => __PACKAGE__.'/'.$VERSION )
     };
     bless \%args,$class;
 }
 
 sub get {
-    my $self = shift;
-    my ($city,$day) = @_;
+    my ($self, $city) = @_;
     croak('city is required') unless $city;
-    $day ||= 'today';
     my $cityid = $self->__get_cityid($city);
 
-    my $res = URI::Fetch->fetch("http://weather.livedoor.com/forecast/webservice/rest/v1?city=$cityid&day=$day", %{$self->{fetch}});
+    my $res = URI::Fetch->fetch(ENDPOINT_URI. "?city=$cityid", %{$self->{fetch}});
     croak("Cannot get weather information : " . URI::Fetch->errstr) unless $res;
 
+    return $self->__parse_forecast($res->content);
+}
+
+sub __parse_forecast {
+    my ($self, $json) = @_;
     my $ref;
-    eval{$ref = XMLin($res->content)};
+    eval{$ref = decode_json($json)};
     croak('Oops! failed reading weather information : ' . $@) if $@;
 
-    #temperature fixing
-    ref $ref->{temperature}{max}{celsius} and $ref->{temperature}{max}{celsius} = undef;
-    ref $ref->{temperature}{min}{celsius} and $ref->{temperature}{min}{celsius} = undef;
-    ref $ref->{temperature}{max}{fahrenheit} and $ref->{temperature}{max}{fahrenheit} = undef;
-    ref $ref->{temperature}{min}{fahrenheit} and $ref->{temperature}{min}{fahrenheit} = undef;
+    ### temperature fixing for null case
+    for ( @{$ref->{forecasts}} ) {
+        ref $_->{temperature}{max}{celsius} and $_->{temperature}{max}{celsius} = undef;
+        ref $_->{temperature}{min}{celsius} and $_->{temperature}{min}{celsius} = undef;
+        ref $_->{temperature}{max}{fahrenheit} and $_->{temperature}{max}{fahrenheit} = undef;
+        ref $_->{temperature}{min}{fahrenheit} and $_->{temperature}{min}{fahrenheit} = undef;
+    }
 
     return $ref;
 }
 
 sub __get_cityid {
     my ($self,$city) = @_;
-    return $city if $city =~ /^\d+$/;
-
-    my $cityname = pack('C0A*',$city);
-    croak('Invalid city name. cannot find city id with ' . $city) 
-	unless exists $self->__forecastmap->{$cityname};
-
-    return $self->__forecastmap->{$cityname};
+    $city =~ /^\d+$/ ? $city : $self->__forecastmap->{$city} or croak('Invalid city name. cannot find city id with '. $city);
 }
 
-sub __forecastmap{
+sub __forecastmap {
     my $self = shift;
-
-    return $self->{forecastmap} if $self->{forecastmap};
-
-    my $res = URI::Fetch->fetch('http://weather.livedoor.com/forecast/rss/forecastmap.xml', %{$self->{fetch}});
-    croak("Couldn't get forecastmap.xml : " . URI::Fetch->errstr) unless $res;
-
-    my $ref;
-    eval{$ref = XMLin($res->content,ForceArray => [qw/pref area city/])};
-    croak('Oops! failed reading forecastmap.xml : ' . $@) if $@;
-
-    my %forecastmap;
-    foreach my $area (@{$ref->{channel}->{'ldWeather:source'}->{area}}){
-	foreach my $pref (@{$area->{pref}}){
-	    $forecastmap{pack('C0A*',$pref->{city}->{$_}->{title})} = $_
-		for keys %{$pref->{city}};
-	}
+    unless ($self->{forecastmap}) {
+        my $res = URI::Fetch->fetch(FORECASTMAP_URI, %{$self->{fetch}});
+        croak("Couldn't get forecastmap: " . URI::Fetch->errstr) unless $res;
+        $self->{forecastmap} = $self->__parse_forecastmap($res->content);
     }
-
-    $self->{forecastmap} = \%forecastmap;
     return $self->{forecastmap};
+}
+
+sub __parse_forecastmap {
+    my ($self, $str) = @_;
+
+    my $ref = eval { XMLin($str, ForceArray => [qw[pref area city]]) };
+    if ($@) {
+        local $Carp::CarpLevel = 1;
+        croak('Oops! failed reading forecastmap: '. $@);
+    }
+    my %forecastmap;
+    foreach my $pref ( @{$ref->{channel}{'ldWeather:source'}{pref}} ){
+        $forecastmap{$pref->{city}{$_}{title}} = $_ for keys %{$pref->{city}};
+    }
+    return \%forecastmap;
 }
 
 1;
 __END__
+
+=encoding utf8
 
 =head1 NAME
 
@@ -85,13 +92,18 @@ WebService::Livedoor::Weather - Perl interface to Livedoor Weather Web Service
 
 =head1 SYNOPSIS
 
+  use utf8;
   use WebService::Livedoor::Weather;
-
+  
+  binmode STDOUT, ':utf8';
+  
   $lwws = WebService::Livedoor::Weather->new;
-  my $ret = $lwws->get('63','tomorrow'); #63 is tokyo
-
-  print $ret->{title};
-  print $ret->{description};
+  
+  my $ret = $lwws->get('東京'); # forecast data for Tokyo.
+  ### or ...
+  $ret = $lwws->get('130010'); # '130010' is Tokyo's city_id. 
+  
+  printf "%s\n---\n%s\n", $ret->{title}, $ret->{description};
 
 =head1 DESCRIPTION
 
@@ -110,13 +122,13 @@ creates an instance of WebService::Livedoor::Weather.
 
 C<fetch> is option for URI::Fetch that used for fetching weather information.
 
-=item get(cityid or name,[day])
+=item get(cityid or name)
 
-    my $ret = $lwws->get('63','tomorrow'); #63 is tokyo
-    my $ret = $lwws->get('cityname','today');
+    my $ret = $lwws->get('63'); #63 is tokyo
+    my $ret = $lwws->get('cityname');
 
 retrieve weather.
-You can get a city id from http://weather.livedoor.com/forecast/rss/forecastmap.xml
+You can get a city id from http://weather.livedoor.com/forecast/rss/primary_area.xml
 
 =head1 SEE ALSO
 
@@ -125,7 +137,9 @@ http://weather.livedoor.com/weather_hacks/webservice.html (Japanese)
 
 =head1 AUTHOR
 
-Masahiro Nagano, E<lt>kazeburo@nomadscafe.jpE<gt>
+Original version by Masahiro Nagano, E<lt>kazeburo@nomadscafe.jpE<gt>
+
+Latest version by Satoshi Azuma, E<lt>ytnobody@gmail.comE<gt>
 
 =head1 COPYRIGHT AND LICENSE
 
