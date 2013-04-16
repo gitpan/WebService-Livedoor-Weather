ab61dd112c8a9b9409783f8616d4ac6cc00c9866
Satoshi Azuma <ytnobody@gmail.com>
2013-04-16 02:08:34 +0000

    Import of YTURTLE/WebService-Livedoor-Weather-0.04 from CPAN.
    gitpan-cpan-distribution: WebService-Livedoor-Weather
    gitpan-cpan-version:      0.04
    gitpan-cpan-path:         YTURTLE/WebService-Livedoor-Weather-0.04.tar.gz
    gitpan-cpan-author:       YTURTLE
    gitpan-cpan-maturity:     released

--- eg/sample.pl
+++ eg/sample.pl
@@ -5,10 +5,8 @@ use utf8;
 use lib "lib";
 use WebService::Livedoor::Weather;
 binmode STDOUT, ':utf8';
-
+ 
 my $lwws = WebService::Livedoor::Weather->new;
 my $ret = $lwws->get(130010);
-
+ 
 printf "%s\n---\n%s\n", $ret->{title}, $ret->{description}{text};
-
-
